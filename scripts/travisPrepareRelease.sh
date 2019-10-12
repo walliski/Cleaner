8313e1758006f3d016392789997c736f980ced53
Walliski <walliski@gmail.com>
2019-10-12 17:33:39 +0300

    Add script for preparing for release

---
+++ scripts/travisPrepareRelease.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+set -x
+
+# Creates a folder with all content that should be included in the release.
+
+mkdir -p staging
+
+cp Release/*.so staging
+cp -r sourcemod/ staging
+
+ls -la staging
